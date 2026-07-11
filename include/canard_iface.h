a3050586c65332f9c05f9c844614e37b2007d2fa
J <jon.joyner123@gmail.com>
2026-07-10 20:59:39 -0400

    added function definitions to canard_iface.h

--- include/canard_iface.h
+++ include/canard_iface.h
@@ -17,12 +17,34 @@
 // include the base canard API
 #include <canard.h>
 
-// we are using the socketcan driver
-#include <socketcan.h>
-
-// include the headers for the generated DroneCAN messages from the
-// dronecan_dsdlc compiler>
-#include <dronecan_msgs.h>
-
 #define MY_NODE_ID 12
+
+class CanardInterface : public Canard::Interface {
+    // friend class CANSimNode;
+    CanardInterface(uint8_t index) : Interface(index_) {}
+public:
+    void init(const char *iface_name);
+    // implement required interface functions
+    bool broadcast(const Canard::Transfer &bcast_transfer) override;
+    bool request(uint8_t dest_node_id, const Canard::Transfer &req_transfer);
+    bool respond (uint8_t dest_node_id, const Canard::Transfer &res_transfer);
+    uint8_t get_node_id() const override { return canard.node_id; };
+    void set_node_id(uint8_t node_id) {
+        canardSetLocalNodeID(&canard, node_id);
+    }
+
+    static void should_accept_transfer(const CanardInstance* inst,
+                                       size_t* out_data_type_signature,
+                                       uint16_t data_type_id,
+                                       CanardTransferType transfer_type,
+                                       uint8_t source_node_id);
+
+private:
+    uint8_t index_;
+    uint8_t memory_pool[2048];
+    CanardInstance canard;
+    CanardTxTransfer tx_transfer;
+
+    // serial_iface;
+};
 
